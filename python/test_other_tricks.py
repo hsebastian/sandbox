bdf6d03707bc22811fc43cfeb6cf1cda824ddd74
Hans Sebastian <hsebastian@splunk.com>
2012-02-29 22:09:56 -0800

    added examples of assigning multiple values and consecutive values

--- python/test_other_tricks.py
+++ python/test_other_tricks.py
@@ -1,7 +1,21 @@
+import types
+
+
 class TestAssignment(object):
     def test_assigning_multiple_values_at_once(self):
-        raise AssertionError
+        comics = ('dono', 'kasino', 'indro')
+        assert len(comics) == 3
+        x, y, z = comics
+        assert x == 'dono'
+        assert y == comics[1]
+        assert z == 'indro'
+
 
     def test_assigning_consecutive_values(self):
-        raise AssertionError
+        length = 6
+        my_list = range(length)
+        assert type(my_list) == types.ListType
+        a, b, c, d, e, f = range(length)
+        assert type(a) == types.IntType
+        assert a == my_list[0]
 
